15ddb79cd5579bca11db6b5d2f0bb57e2386ccc9
Hitesh <hitgo17@gmail.com>
2020-04-25 23:20:46 +0530

    updated readme
    Signed-off-by: Hitesh <hitgo17@gmail.com>

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <header className="App-header">
         <PrimarySearchAppBar />
         <PostCard />
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
